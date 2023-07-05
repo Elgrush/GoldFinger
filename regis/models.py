a4afbc51c8516399c124ba10f1a515c28a472ef2
Никита <nikikarp@mail.ru>
2023-07-05 17:58:45 +0300

    Starting dev

--- regis/models.py
+++ regis/models.py
@@ -5,23 +5,25 @@ from django import forms
 
 # Create your models here.
 class ArticleRequestForm(forms.ModelForm):
-    field_order = ["profile"]
-    profile = forms.CharField()
+    field_order = ["user"]
+    user = forms.CharField()
 
     class Meta:
         model = ArticleRequest
         fields = ["article", "size", "amount", "factory"]
 
+    factory = forms.CharField()
+
     def show(self, model):
         for field in self.fields:
             self.fields[field].widget.attrs['readonly'] = True
-        self.initial['profile'] = model.profile
+        self.initial['user'] = model.user
         self.initial['article'] = model.article
         self.initial['size'] = model.size
         self.initial['amount'] = model.amount
         self.initial['factory'] = model.factory
 
-        self.fields['profile'].label = "Ник:"
+        self.fields['user'].label = "Ник:"
         self.fields['article'].label = "Артикул:"
         self.fields['size'].label = "Размер:"
         self.fields['amount'].label = "Количество:"
